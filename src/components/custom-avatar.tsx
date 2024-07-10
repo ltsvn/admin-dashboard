9ae0209fa532888aff5845bbdbb1c2f252189f3c
ltsvn <92997990+ltsvn@users.noreply.github.com>
2024-07-10 17:32:44 +0200

    user avatar setup

---
+++ src/components/custom-avatar.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Avatar as AntdAvatar, AvatarProps } from "antd";
+import { getNameInitials } from "@/utilities";
+
+type Props = AvatarProps & {
+  name: string | undefined;
+};
+const CustomAvatar = ({ name, style, ...rest }: Props) => {
+  return (
+    <AntdAvatar
+      alt={name}
+      size="small"
+      style={{
+        background: "#87d068",
+        display: "flex",
+        alignItems: "center",
+        border: "none",
+        ...style,
+      }}
+      {...rest}
+    >
+      {getNameInitials(name || "")}
+    </AntdAvatar>
+  );
+};
+
+export default CustomAvatar;
--- src/components/layout/current-user.tsx
+++ src/components/layout/current-user.tsx
@@ -1,19 +1,79 @@
-import React from 'react';
-import {Popover} from "antd";
+import React from "react";
 
-function CurrentUser() {
-    return (
-        <>
-        <Popover
-            placement='bottomRight'
-            trigger='click'
-            overlayInnerStyle={{padding: 0}}
-            overlayStyle={{zIndex:999}}
+import { useGetIdentity } from "@refinedev/core";
+
+import { SettingOutlined } from "@ant-design/icons";
+import { Button, Popover } from "antd";
+
+import type { User } from "@/graphql/schema.types";
+import CustomAvatar from "@/components/custom-avatar";
+// import { AccountSettings } from "../account-settings";
+import { Text } from "@/components/text";
+
+export const CurrentUser = () => {
+  const [opened, setOpened] = React.useState(false);
+  const { data: user } = useGetIdentity<User>();
+
+  const content = (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <Text
+        strong
+        style={{
+          padding: "12px 20px",
+        }}
+      >
+        {user?.name}
+      </Text>
+      <div
+        style={{
+          borderTop: "1px solid #d9d9d9",
+          padding: "4px",
+          display: "flex",
+          flexDirection: "column",
+          gap: "4px",
+        }}
+      >
+        <Button
+          style={{ textAlign: "left" }}
+          icon={<SettingOutlined />}
+          type="text"
+          block
+          onClick={() => setOpened(true)}
         >
-            test
-        </Popover>
-        </>
-    );
-}
+          Account settings
+        </Button>
+      </div>
+    </div>
+  );
 
-export default CurrentUser;
+  return (
+    <>
+      <Popover
+        placement="bottomRight"
+        content={content}
+        trigger="click"
+        overlayInnerStyle={{ padding: 0 }}
+        overlayStyle={{ zIndex: 999 }}
+      >
+        <CustomAvatar
+          name={user?.name}
+          src={user?.avatarUrl}
+          size="default"
+          style={{ cursor: "pointer" }}
+        />
+      </Popover>
+      {/*{user && (*/}
+      {/*    <AccountSettings*/}
+      {/*        opened={opened}*/}
+      {/*        setOpened={setOpened}*/}
+      {/*        userId={user.id}*/}
+      {/*    />*/}
+      {/*)}*/}
+    </>
+  );
+};
